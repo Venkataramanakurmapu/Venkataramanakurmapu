c1aedb9b07f6cac0060b855024e2ad782b53b6b8
venkataramanakurmapu <venkataramanakurmapu3@gmail.com>
2021-07-17 13:14:55 +0530

    admin admin login

--- js/login.js
+++ js/login.js
@@ -9,7 +9,7 @@ var login_success = false; /* set this to true if the login was a success */
 function validate() {
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
-  if ((username == "DORA" && password == "DORA")||(username == "JYOTHI" && password == "JYOTHI")||(username == "MOUNIKA" && password == "MOUNIKA")||(username == "PRASAD" && password == "PRASAD")||(username == "Venkki" && password == "Venkki")||(username == "HEMANTH" && password == "HEMANTH")||(username == "SIREESHA" && password == "SIREESHA")||(username == "PAVANTEJA" && password == "PAVANTEJA")) {
+  if ((username == "DORA" && password == "DORA")||(username == "ADMIN" && password == "ADMIN")||(username == "JYOTHI" && password == "JYOTHI")||(username == "MOUNIKA" && password == "MOUNIKA")||(username == "PRASAD" && password == "PRASAD")||(username == "Venkki" && password == "Venkki")||(username == "HEMANTH" && password == "HEMANTH")||(username == "SIREESHA" && password == "SIREESHA")||(username == "PAVANTEJA" && password == "PAVANTEJA")) {
     login_success = true;
   }
   
