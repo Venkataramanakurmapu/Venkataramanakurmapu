b685a715270b722694b9f33d67a50e05b21c47fa
venkataramanakurmapu <venkataramanakurmapu3gmail.com>
2021-07-17 09:33:10 +0530

    password update

--- js/login.js
+++ js/login.js
@@ -9,7 +9,7 @@ var login_success = false; /* set this to true if the login was a success */
 function validate() {
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
-  if (username == "Admin" && password == "Admin") {
+  if ((username == "DORA" && password == "DORA")||(username == "JYOTHI" && password == "JYOTHI")||(username == "MOUNIKA" && password == "MOUNIKA")||(username == "PRASAD" && password == "PRASAD")||(username == "Venkki" && password == "Venkki")||(username == "HEMANTH" && password == "HEMANTH")||(username == "SIREESHA" && password == "SIREESHA")||(username == "PAVANTEJA" && password == "PAVANTEJA")) {
     login_success = true;
   }
   
